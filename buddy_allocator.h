fd90d7dccfd3c17924dee68562d18f18419ca277
primeh0 <pmehta11@illinois.edu>
2014-11-13 10:43:56 -0600

    Create buddy_allocator.h

---
+++ buddy_allocator.h
@@ -0,0 +1,27 @@
+struct mem_buddy{
+  unsigned long addr;
+  unsigned long order;  
+  unsigned long min_size;
+  unsigned long number_blocks; 
+  unsigned long bit_availability; 
+  
+  struct list_head *avail_blocks;
+  };
+  
+ struct mem_buddy *
+mem_init(
+unsigned long addr,
+unsigned long order,
+unsigned long min_size
+);
+
+malloc(
+struct mem_buddy *mb,
+unsigned long order
+);
+
+free(
+struct mem_buddy *mb,
+void *addr,
+unsigned long order
+);
