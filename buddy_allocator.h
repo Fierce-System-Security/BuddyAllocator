48152ec96f9c5fbf2a969b6fc3aa6c4c5f6c41d4
rr1510 <rrui1510@gmail.com>
2014-11-20 23:03:44 -0600

    Update buddy_allocator.h

--- buddy_allocator.h
+++ buddy_allocator.h
@@ -3,6 +3,7 @@
 
 #include <linux/list.h>
 
+public:
 typedef struct _mem_buddy{
   unsigned long addr;
   unsigned long order;  
@@ -20,15 +21,23 @@ unsigned long order,
 unsigned long min_size
 );
 
-malloc(
+void * malloc(
 struct mem_buddy *mb,
 unsigned long order
 );
 
-free(
+void free(
 struct mem_buddy *mb,
 void *addr,
 unsigned long order
 );
+
+private:
+	size_t next_power_2(size_t size);
+	unsigned long address_index(struct mem_buddy *mb, struct block *block);
+	void mark_free(struct mem_buddy *mb, struct block *block);
+	void mark_allocated(struct mem_buddy *mb, struct block *block);
+	int availablity(void *block);
+	void findBuddy(struct mem_buddy *mb, size_t size);
 
 #endif
